8311f6d857cf8564e220332d048071cbd941c443
Tincho-91 <martin_baryuk@outlook.com>
2023-10-19 00:03:16 -0300

    comienzo del trabajo mercado liebre

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.listen(3000,()=>{
+    console.log("servidor ejecutandose en el puerto 3000")
+})
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/home.html"));
+});
+
+app.use(express.static("public"));
